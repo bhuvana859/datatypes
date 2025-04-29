9c7f077ff2bdb4bf3611f66715031a535d293b07
charanb15 <basavar05@gmail.com>
2025-04-30 00:01:47 +0530

    operators practice

---
+++ e2e/JSpractice/operators.js
@@ -0,0 +1,323 @@
+
+
+//Conatination and coersion  //
+//==================================================
+// x = 10
+
+// y = 20
+
+// z= x+y
+
+//console.log(z)
+//ans 30
+
+// x = 10
+
+// y = "Raju"
+
+// z= x+y
+
+// console.log(z)
+
+//ans 10Raju
+
+
+// x = 10
+
+// y = "10"
+
+// z= x+y
+
+// console.log(z)
+
+//ans1010
+
+// x = "10"
+
+// y = "10"
+
+// z= x+y
+
+//console.log(z)
+
+//ans1010
+//its called concatination bot string will be added
+
+// x = 10
+
+// y = 20
+
+// z= x-y
+
+// console.log(z)
+
+// ans -10
+
+
+// x = 40
+
+// y = 20
+
+// z= x-y
+
+// console.log(z)
+//ans 20
+
+// x = 40
+
+// y = "20"
+
+// z= x-y
+
+// console.log(z)
+
+//ans 20
+ //its called coersion string will be converted to number
+
+
+// // % Modulusoperators-[or] Arthemetic operators reminder  //
+// // ==================================================
+
+
+// // <condition> ? <value1> : <value2>;
+
+// // // 1. if condition is true then value1 will be assigned to finalstu
+
+
+// //example 1
+//  x= "raju"
+//  y="Arjun"
+//  z= 2=="2" ? x : y
+//  console.log(z)
+
+// //ans raju
+
+// //example 2
+   
+//    x= "raju"
+//    y="Arjun"
+//    z= 2==="2" ? x : y
+//    console.log(z)
+// //ans Arjun
+
+// //if condition is false then value2 will be assigned to finalstu
+// //if condition is true then value1 will be assigned to finalstu
+
+
+// //example 3
+
+// Workers = 4>6 ? 2:1
+
+// console.log(Workers)       
+// //ans 1
+
+// //example 4
+// Workers =4>3 ? 2:1
+// console.log(Workers)
+// //ans 2
+
+// //GREENCART TOTAL VALUE CONCEPT
+
+// price1=120
+// price2=60
+// total=180
+
+// console.log(price1+price2==total)
+// //ans true  
+
+// // vVEHICLE SALSE STATUS CONCEPT
+
+// vehiclesoldstatus="sold"
+// vehiclesoldstatus="inprogress"
+// payment=true
+// status_of_vehicle=payment ? "sold" :inprogress
+// console.log(status_of_vehicle)
+// //ans sold
+
+
+// vehiclesoldstatus="sold"
+// vehiclesoldstatus="inprogress"
+// payment=false
+// status_of_vehicle=payment ? "sold" :"inprogress"
+// console.log(status_of_vehicle)
+
+// //ans inprogress
+
+// // realtime assertion concept
+
+// payment=true
+// status_of_vehicle=payment ? "sold" :"inprogress"
+// console.log(status_of_vehicle)
+
+// //syntax
+// await xpect (sold).to.be(status_of_vehicle)
+
+//assertion will be pass
+
+
+//arthemetic operators //  +, -, *, /, %
+
+// //=================================
+
+
+//  val1 = "40"
+
+//  val2 = "20"
+
+// val3 = val1-val2
+
+// console.log(val3)
+
+// console.log(4+5)
+
+// a=20
+// b=30
+// c=a+b
+// console.log(c)
+
+// console.log(45-25)
+
+// console.log(4*5)
+
+// console.log(8/4)
+
+
+// val1=40
+// val2="20"
+// val3=val1-val2
+
+// console.log(val3)
+
+// val1=40
+// val2="raju"
+// val3=val1-val2
+
+// console.log(val3)
+// //ans NaN
+// val1="40"
+// val2="20"
+// val3=val1-val2
+
+// console.log(val3)
+
+
+//ans 20 coersion will happen
+
+
+//INCEMENT AND DECEMNT OPERATORS //
+//=================================
+
+//++  increment operator
+//--  decrement operator
+
+
+// X=20
+// X++
+// console.log(X)
+
+
+
+// X=20
+// X--
+// console.log(X)
+
+//comparison operators //
+//=================================
+// == is compar only values not datatypes
+// === is compar values and datatypes
+
+// "Raju"=="Raju" 
+// //true
+
+// console.log(2==2)
+// //true
+// console.log(2===2)
+// //true
+// console.log(2=="2")
+// //true   
+// console.log(2==="2")
+// //false
+
+//logical operators //
+//=================================
+
+// //   AND 
+
+//   1  1  1
+
+//   1  0  0
+
+//   0  1  0
+
+//   0  0  0
+
+
+console.log((4>5) && (3=="3"))
+
+
+console.log((4<5) && (3=="3"))
+
+
+console.log((4>5) && (3=="3"))
+
+
+console.log(2==2) && (4>3)
+
+//    OR 
+
+//    1 0  1
+
+//    0 1  1
+//    0 0  0
+//    1 1  1
+
+
+console.log((4<5) || (3=="3"))
+
+console.log((4>5) && (3==="3"))
+
+
+// // NOT 
+
+// // 1 0
+// // 0 1
+
+ console.log(!(4>5) )
+ 
+ console.log(!(4<5) )
+
+
+//asignment operators //
+//=================================
+
+
+x=20
+y=6
+x=y
+console.log(x)
+
+x=6
+x+=3
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
