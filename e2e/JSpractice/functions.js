61186860362e4d330a62457894fd4b9094fc954a
charanb15 <basavar05@gmail.com>
2025-05-01 17:09:11 +0530

    functions and objects

---
+++ e2e/JSpractice/functions.js
@@ -0,0 +1,191 @@
+
+//FUNCTIONS
+//==========================
+
+//2.ARGUMENTS   WITHOUT PARAMETERS
+//3.FUNCTIONS WITH RETURN VALUES
+
+
+// based on condition 
+
+// if (ture){
+
+
+// }
+
+// for while  do while 
+
+// for (let i=1; i<=10 ; i++){
+
+
+// }
+
+
+// function add(){
+
+//     console.log(2)
+//     console.log(5)
+//     console.log(2+5)
+// }
+
+
+// add()
+
+
+
+//1.ARGUMENTS   WITH PARAMETERS
+
+
+// function adition (x,y)
+// {
+
+//     console.log(x)
+//     console.log(y)
+//     console.log(x+y)
+
+// }
+// adition(2,2)
+
+
+// function add(num1, num2) {
+
+//     console.log(num1)
+//     console.log(num2)
+//     console.log(num1 + num2)
+// }
+
+
+// add(3,7)
+
+// add(8,3)
+
+// functions without params 
+
+
+
+
+// functions with Parameters
+//====================================================== 
+
+// function multiply(){
+
+//     // console.log(4)
+//     // console.log(5)
+
+//     return 4*5
+// }
+
+
+
+// function multiply(num1, num2){
+
+//     return num1*num2
+// }
+
+// x = multiply(3,5)
+
+// console.log(x)
+
+
+
+
+
+// function multiply(num1, num2){
+
+//     return num1*num2
+// }
+
+// x = multiply(10,10)
+
+// console.log(x)
+
+
+// functions without params 
+
+// functions with Parameters 
+
+// functions without params  and with return 
+  
+// functions with Parameters and with retuen 
+
+//POM - Page Object Model 
+
+//es6 - Arrow functions 
+
+
+
+// function add(){
+
+//     console.log(2)
+//     console.log(5)
+//     console.log(2+5)
+// }
+
+// add()
+
+// add = () => {
+ 
+//     console.log(2)
+//     console.log(5)
+//     console.log(2+5)
+
+// }
+
+
+// add()
+
+
+// function add(num1, num2) {
+
+//     console.log(num1)
+//     console.log(num2)
+//     console.log(num1 + num2)
+// }
+
+
+// add = (num1, num2) => {
+  
+//     console.log(num1)
+//     console.log(num2)
+//     console.log(num1 + num2)
+
+// }
+
+// add(5,8)
+
+
+// function squarerootofNumber(num){
+
+//     return num*num
+// }
+
+
+
+// squarerootofNumber = num => {
+
+//     console.log(num*num)
+
+// }
+
+// functionname = () =>{
+
+
+// }  // When no params 
+
+// functionname = (param1, param2, pafram2) =>{
+
+
+// }  //  when Multiple params
+
+// functionname = param1 => {
+
+
+// } // when you have single param
+
+
+// function squarerootofNumber(num){
+
+//     return num*num
+// }
+
+// squarerootofNumber = num => num*num
